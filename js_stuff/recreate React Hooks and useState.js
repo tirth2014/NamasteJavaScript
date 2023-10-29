9aae4c7b2bb897637d9ec0102467c4f65da75afd
Tirth Patel <tirth99092@gmail.com>
2023-10-29 18:14:38 +0530

    Update recreate React Hooks and useState.js

--- js_stuff/recreate React Hooks and useState.js
+++ js_stuff/recreate React Hooks and useState.js
@@ -73,3 +73,103 @@ console.log(add()); // 2
 console.log(add()); // 3
 console.log(add()); // 4
 
+
+
+// Let's implement useState hook.
+
+function useState(initVal) {
+  let _val = initVal;
+  const state = _val;
+  const setState = (newVal) => {
+    _val = newVal;
+  };
+
+  return [state, setState];
+}
+
+const [count, setCount] = useState(1);
+
+console.log(count); // 1
+setCount(2);
+console.log(count); // 1
+
+
+// one simple (but cheap) way to fix it is to make state a getter function.
+
+function useState(initVal) {
+  let _val = initVal;
+  const state = () => _val;
+  const setState = (newVal) => {
+    _val = newVal;
+  };
+
+  return [state, setState];
+}
+
+const [count, setCount] = useState(1);
+
+console.log(count()); // 1
+setCount(2);
+console.log(count()); // 2
+
+
+// does the same thing, just introduced React module
+const React = (function () {
+  function useState(initVal) {
+    let _val = initVal;
+    const state = () => _val;
+    const setState = (newVal) => {
+      _val = newVal;
+    };
+
+    return [state, setState];
+  }
+  return { useState };
+})();
+
+const [count, setCount] = React.useState(1);
+
+console.log(count()); // 1
+setCount(2);
+console.log(count()); // 2  
+
+
+
+// Let's also introduce Component and render:
+
+const React = (function () {
+  let _val;
+  function useState(initVal) {
+    const state = _val || initVal;
+    const setState = (newVal) => {
+      _val = newVal;
+    };
+
+    return [state, setState];
+  }
+
+  // teach react what's render
+  function render(Component) {
+    const C = Component();
+    C.render();
+    return C;
+  }
+
+  return { useState, render };
+})();
+
+function Component() {
+  const [count, setCount] = React.useState(1);
+  return {
+    render: () => console.log(count),
+    click: () => setCount(count + 1)
+  };
+}
+
+var App = React.render(Component); // 1
+App.click();
+var App = React.render(Component); // 2
+App.click();
+var App = React.render(Component); // 3
+App.click();
+var App = React.render(Component); // 4
